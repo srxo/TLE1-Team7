d2b658170d4437f1da7add736ce0e1497c0167ce
Jeffrey <jeffrey.otterloo@gmail.com>
2023-11-13 19:02:40 +0100

    Created a create function for the game model

---
+++ app/Models/Game.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'devices',
+        'banner_image',
+    ];
+}
