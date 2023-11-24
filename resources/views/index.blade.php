0a1e99dd7b31aa4f210e19b71007e5ee6ef80f6c
AntonieBananza <antbij@outlook.com>
2023-11-24 16:55:26 +0100

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JSON Search</title>
+    <title>Database search Search</title>
 </head>
 <body>
 <form action="{{ route('search') }}" method="post">
