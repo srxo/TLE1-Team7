2cedf91ef02a030ad19a727828cb4b9934650708
Jeffrey <jeffrey.otterloo@gmail.com>
2024-03-04 19:18:15 +0100

    Home message now correct

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Welcome to Sightless Gamers!') }}
                 </div>
             </div>
         </div>
